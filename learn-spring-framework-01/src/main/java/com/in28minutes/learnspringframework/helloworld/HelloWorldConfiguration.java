43e0d6d8781613e5cc67429f33aec6c40d03b370
Utkarsh <PRAS432118@exactsoftware.com>
2026-01-29 18:03:44 +0530

    Fist commit

---
+++ learn-spring-framework-01/src/main/java/com/in28minutes/learnspringframework/helloworld/HelloWorldConfiguration.java
@@ -0,0 +1,61 @@
+package com.in28minutes.learnspringframework.helloworld;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+//Eliminate verbosity in creating Java Beans
+//Public accessor methods, constructor,
+//equals, hashcode and toString are automatically created.
+//Released in JDK 16.
+
+record Person (String name, int age, Address address) { };
+
+//Address - firstLine & city
+record Address(String firstLine, String city){ };
+
+@Configuration
+public class HelloWorldConfiguration {
+
+    @Bean
+    public String name() {
+        return "Ranga";
+    }
+
+    @Bean
+    public int age() {
+        return 15;
+    }
+
+    @Bean
+    @Primary
+    public Person person() {
+        return new Person("Ravi", 20, new Address("Main Street", "Utrecht"));
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address2) {
+        return new Person(name, age, address2);
+    }
+
+    @Bean
+    public Address address2() {
+        return new Address("address2", "London");
+    }
+
+    @Bean(name = "address2")
+    public Address address() {
+        return new Address("Baker Street", "London");
+    }
+
+    @Bean(name = "address3")
+    public Address address3() {
+        return new Address("Motinagar", "Hyderabad");
+    }
+
+}

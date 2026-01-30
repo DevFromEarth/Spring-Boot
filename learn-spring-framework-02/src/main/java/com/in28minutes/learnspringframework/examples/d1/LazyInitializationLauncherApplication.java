bf43036c273d1107bd9e22937d4ac8d0d0a070ec
Utkarsh <PRAS432118@exactsoftware.com>
2026-01-30 13:17:31 +0530

    LazyInitialization and scopes

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/d1/LazyInitializationLauncherApplication.java
@@ -0,0 +1,46 @@
+package com.in28minutes.learnspringframework.examples.d1;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class ClassA {
+
+}
+
+@Component
+@Lazy
+class ClassB {
+    private ClassA classA;
+
+    public ClassB(ClassA classA) {
+        System.out.println("ClassB Constructor");
+        this.classA = classA;
+    }
+
+    public void doSomething()
+    {
+        System.out.println("inside doSomething");
+    }
+}
+
+@Configuration
+@ComponentScan
+public class LazyInitializationLauncherApplication {
+
+    public static void main(String[] args) {
+
+        try(var context = new AnnotationConfigApplicationContext(LazyInitializationLauncherApplication.class)){
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+            System.out.println("----------------------------------");
+
+            context.getBean(ClassB.class).doSomething();
+        }
+    }
+}
